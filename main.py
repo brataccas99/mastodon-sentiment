33a7c34bad9edb04c3de82c48b3605993a2927f1
Alessio Rizzolo <aless.rizzolo@gmail.com>
2023-07-04 21:23:28 +0200

    Added env varibale and modified output

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 import nltk
+from bs4 import BeautifulSoup
 from mastodon import Mastodon
+from dotenv import dotenv_values
 from nltk.sentiment import SentimentIntensityAnalyzer
 
 nltk.download('vader_lexicon')
 
+env = dotenv_values()
+
 # Create an instance of the Mastodon class and authenticate
 mastodon = Mastodon(
-    access_token='',
+    access_token=env['ACCESS_TOKEN'],
     api_base_url='https://mastodon.uno'
 )
 
@@ -16,14 +20,18 @@ timeline = mastodon.timeline_home()
 # Create an instance of the SentimentIntensityAnalyzer class
 analyzer = SentimentIntensityAnalyzer()
 
-# Iterate over the timeline posts and perform sentiment analysis
+# Loop through the timeline statuses
 for status in timeline:
     content = status['content']
     author = status['account']['username']
     timestamp = status['created_at']
 
-    # Perform sentiment analysis on the post content
-    sentiment_scores = analyzer.polarity_scores(content)
+    # Use BeautifulSoup to remove HTML tags from content
+    soup = BeautifulSoup(content, 'html.parser')
+    filtered_content = soup.get_text()
+
+    # Perform sentiment analysis on the filtered content
+    sentiment_scores = analyzer.polarity_scores(filtered_content)
     sentiment_score = sentiment_scores['compound']
 
     # Classify sentiment based on the sentiment score
@@ -36,6 +44,6 @@ for status in timeline:
 
     # Print the post information and sentiment analysis result
     print(f"Author: {author}")
-    print(f"Content: {content}")
+    print(f"Content: {filtered_content}")
     print(f"Sentiment: {sentiment_label} ({sentiment_score})")
     print("---")
